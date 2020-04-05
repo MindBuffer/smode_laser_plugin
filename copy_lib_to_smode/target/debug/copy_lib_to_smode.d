cb19aa7d605ea5a09df4edd30387ee6b5c5aa4ad
JoshuaBatty <joshpbatty@gmail.com>
2020-04-05 13:36:24 +0200

    new program for copying header and lib files to SmodeSDK

---
+++ copy_lib_to_smode/target/debug/copy_lib_to_smode.d
@@ -0,0 +1,1 @@
+D:\rust\nannou_projects\smode_laser_plugin\copy_lib_to_smode\target\debug\copy_lib_to_smode.exe: D:\rust\nannou_projects\smode_laser_plugin\copy_lib_to_smode\src\main.rs
---
+++ copy_lib_to_smode/target/debug/deps/copy_lib_to_smode.d
@@ -0,0 +1,5 @@
+D:\rust\nannou_projects\smode_laser_plugin\copy_lib_to_smode\target\debug\deps\copy_lib_to_smode.exe: src\main.rs
+
+D:\rust\nannou_projects\smode_laser_plugin\copy_lib_to_smode\target\debug\deps\copy_lib_to_smode.d: src\main.rs
+
+src\main.rs:
