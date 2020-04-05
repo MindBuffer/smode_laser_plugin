8dd4d972ad15b1776b7f008f381ae0b48f53f6a3
JoshuaBatty <joshpbatty@gmail.com>
2020-04-05 18:48:02 +0200

    running cargo fmt

--- copy_plugin_to_smode/src/main.rs
+++ copy_plugin_to_smode/src/main.rs
@@ -1,12 +1,12 @@
-//! This program checks to see if there are any valid release or debug builds of the plugin 
+//! This program checks to see if there are any valid release or debug builds of the plugin
 //! If successful, it copies the .dll files into the Plugins directory of SmodeSDK/Plugins
-//! 
+//!
 //! The program takes an optional command line argument which specifies the Path to the SmodeSDK
 //! folder located on you system. If no argument is supplied, it assumes your SmodeSDK folder
 //! is located at C:\Program Files (x86)\SmodeTech\Smode SDK
-use std::path::Path;
 use std::env;
 use std::fs;
+use std::path::Path;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -26,17 +26,26 @@ fn main() {
 
     let manifest_dir = env!("CARGO_MANIFEST_DIR");
     // This is the folder where our .h file lives that we need to copy
-    let plugin_build_dir = Path::new(manifest_dir).parent().unwrap().join("plugin").join("build").join("Visual Studio 15 2017");
+    let plugin_build_dir = Path::new(manifest_dir)
+        .parent()
+        .unwrap()
+        .join("plugin")
+        .join("build")
+        .join("Visual Studio 15 2017");
 
     dbg!(&plugin_build_dir);
-    let release_build = Path::new(&plugin_build_dir).join("Release").join("Laser.dll");
+    let release_build = Path::new(&plugin_build_dir)
+        .join("Release")
+        .join("Laser.dll");
     if !release_build.exists() {
         println!("Can not find a release build to copy!");
     } else {
         fs::copy(release_build, smode_plugins_dir.join("Laser.dll")).unwrap();
     }
 
-    let debug_build = Path::new(&plugin_build_dir).join("Debug").join("Laser-debug.dll");
+    let debug_build = Path::new(&plugin_build_dir)
+        .join("Debug")
+        .join("Laser-debug.dll");
     if !debug_build.exists() {
         println!("Can not find a debug build to copy!");
     } else {
