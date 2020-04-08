8e5e1739eec2f01855ba58a95074d2be23637385
JoshuaBatty <joshpbatty@gmail.com>
2020-04-08 13:17:15 +0200

    merges in the code from the francis PR

---
+++ plugin/LaserDeviceSelector.h
@@ -0,0 +1,24 @@
+/* -------------------------------------- . ---------------------------------- .
+| Filename : LaserDeviceSelector.h        |                                    |
+| Author   : Francis Maes                 |                                    |
+| Started  : 07/04/2020 08:06             |                                    |
+` --------------------------------------- . --------------------------------- */
+
+#ifndef SMODE_LASER_DEVICE_SELECTOR_H_
+# define SMODE_LASER_DEVICE_SELECTOR_H_
+
+#include "LaserLibrary.h"
+
+namespace smode
+{
+  class LaserDeviceSelector : public TypedDeviceSelector<LaserDevice>
+  {
+  public:
+    LaserDeviceSelector() {}
+
+    OIL_ABSTRACT_OBJECT(LaserDeviceSelector);
+  };
+
+}; /* namespace smode */
+
+#endif // !SMODE_LASER_DEVICE_SELECTOR_H_
