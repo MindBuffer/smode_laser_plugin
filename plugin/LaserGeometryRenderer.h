04d0b5598f10d5240f5af89d3a6030c1ae696d44
JoshuaBatty <joshpbatty@gmail.com>
2020-04-15 12:56:42 +0200

    copy francis filtering of points code

--- plugin/LaserGeometryRenderer.h
+++ plugin/LaserGeometryRenderer.h
@@ -14,50 +14,95 @@
 namespace smode
 {
 
-  class LaserGeometryRenderer : public GeometryLayerUser
+class LaserGeometryRenderer : public GeometryLayerUser
+{
+public:
+  LaserGeometryRenderer() {}
+
+  // GeometryLayerUser
+  bool usesGeometryType(GeometryType::Enum type) const override
+    {return type == GeometryType::lines || type == GeometryType::points;}
+
+  void renderGeometry(GraphicsRenderer& renderer, LayerInterpreter& interpreter, const Geometry& geometry, double opacity) override
   {
-  public:
-    LaserGeometryRenderer() {}
+    if (!inputGeometry.compute(renderer, geometry))
+      return;
 
-    // GeometryLayerUser
-    bool usesGeometryType(GeometryType::Enum type) const override
-      {return type == GeometryType::lines || type == GeometryType::points;}
+    String failureReason;
+    if (!inputGeometry.downloadToRam(renderer.getGraphics(), downloadedPoints, failureReason))
+      return;
 
-    void renderGeometry(GraphicsRenderer& renderer, LayerInterpreter& interpreter, const Geometry& geometry, double opacity) override
+    if (!downloadedPoints.size())
+      return;
+
+    LaserDevice* laserDevice = device.getDevice();
+    if (!laserDevice)
+      return;
+
+    points.clear();
+    computeFinalPoints(downloadedPoints, points);
+    laserDevice->addPoints(points);
+  }
+
+  // Element
+  int64_t computeVersion() const override
+    {return (int64_t)getCurrentFrameNumber();}
+
+  OIL_OBJECT(LaserGeometryRenderer);
+
+protected:
+  typedef LaserDevice::Point Point;
+
+  LaserDeviceSelector device;
+  std::vector<Point> downloadedPoints;
+  std::vector<Point> points;
+
+  static bool isSamePosition(float a, float b)
+    {return a == b;}
+
+  static bool isSamePosition(const glm::vec2& a, const glm::vec2& b)
+    {return isSamePosition(a.x, b.x) && isSamePosition(a.y, b.y);}
+
+  static bool isSameColor(float a, float b)
+    {return a == b;}
+
+  static bool isSameColor(const glm::vec3& a, const glm::vec3& b)
+    {return isSameColor(a.r, b.r) && isSameColor(a.g, b.g) && isSameColor(a.b, b.b);}
+
+  static bool isSameColorAndPosition(const Point& a, const Point& b)
+    {return isSamePosition(a.position, b.position) && isSameColor(a.color, b.color);}
+
+  static Point withBlack(const Point& p)
+    {Point res = p; res.color = glm::vec3(0.f); return res;}
+
+  void computeFinalPoints(const std::vector<Point>& points, std::vector<Point>& res)
+  {
+    jassert(points.size() % 2 == 0);
+    if (points.size() < 2)
+      return;
+    res.push_back(points[0]);
+    res.push_back(points[1]);
+    for (size_t i = 2; i < points.size(); i += 2)
     {
-      if (!inputGeometry.compute(renderer, geometry))
-        return;
-
-      String failureReason;
-      if (!inputGeometry.downloadToRam(renderer.getGraphics(), points, failureReason))
-        return;
-
-      if (!points.size())
-        return;
-      
-      LaserDevice* laserDevice = device.getDevice();
-      if (!laserDevice)
-        return;
-
-      laserDevice->addPoints(points);
+      const Point& previousB = points[i - 1];
+      const Point& currentA = points[i];
+      const Point& currentB = points[i + 1];
+      if (!isSamePosition(previousB.position, currentA.position))
+      {
+        res.push_back(withBlack(previousB));
+        res.push_back(withBlack(currentA));
+      }
+      else if (!isSameColor(previousB.color, currentA.color))
+        res.push_back(currentA);
+      res.push_back(currentB);
     }
+  }
 
-    // Element
-    int64_t computeVersion() const override
-      {return (int64_t)getCurrentFrameNumber();}
+  LaserInputGeometry inputGeometry; // not introspected
 
-    OIL_OBJECT(LaserGeometryRenderer);
-
-  protected:
-    LaserDeviceSelector device;
-    std::vector<LaserDevice::Point> points;
-
-    LaserInputGeometry inputGeometry; // not introspected
-
-  private:
-    typedef GeometryLayerUser BaseClass;
-  };
-
+private:
+  typedef GeometryLayerUser BaseClass;
+};
 
 }; /* namespace smode */
 
