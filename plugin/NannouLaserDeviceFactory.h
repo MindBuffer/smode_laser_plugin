2718e1fd6e5b8f43dae208c42f60eb4fd9fa7649
Qlex42 <alexandre.buge@gmail.com>
2020-11-16 16:38:51 +0100

    * fix: utf8 conversion for (dlrd/Smode-Issues#4611)

--- plugin/NannouLaserDeviceFactory.h
+++ plugin/NannouLaserDeviceFactory.h
@@ -11,6 +11,10 @@
 #include "NannouLaserDeviceIdentifier.h"
 #include "NannouLaserDevice.h"
 
+#ifdef WIN32
+# include <windows.h> // for FormatMessage
+#endif // WIN32
+
 namespace smode
 {
 String macAddressToString(uint8_t mac_address[6])
@@ -38,14 +42,32 @@ public:
   bool isEnumerable() const override
     {return true;}
 
+
+  static String systemErrorToString(const char* systemError)
+  {
+    if (!systemError)
+      return {};
+    int length = static_cast<int>(strlen(systemError));
+#ifdef WIN32
+    if (juce::CharPointer_UTF8::isValidString(systemError, length))
+      return String::fromUTF8(systemError, length);
+
+    WCHAR wideVersion[512] = {};
+    MultiByteToWideChar(CP_ACP, 0, systemError, length, wideVersion, 512);
+    return wideVersion;
+#else // WIN32
+    return String(systemError);
+#endif // WIN32
+  }
+
   bool initializeFactory(Engine& engine, String& failureReason) override
   {
     float dac_timeout_secs = 1.5;
     laser::api_new(&api);
     laser::Result res = laser::detect_dacs_async(&api, dac_timeout_secs, &dac_detector);
-    if (res != laser::Result::Success) {
-      const char* err = laser::api_last_error(&api);
-      failureReason = "Failed to spawn DAC detection thread: " + String(err);
+    if (res != laser::Result::Success) 
+    {
+      failureReason = StringRef("Failed to spawn DAC detection thread: ") + systemErrorToString(laser::api_last_error(&api));
       dac_detector = { 0, };
       return false;
     }
